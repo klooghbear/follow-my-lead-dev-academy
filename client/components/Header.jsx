f37aefbd3ede5c377fe2b34107be403a586e1c9c
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2020-04-12 22:27:29 +1200

    add navbar and initial layout

---
+++ client/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'reactstrap'
+
+const Header = () => {
+  return (
+    <div>
+      <Container className="home-page-headline text-center">
+        Follow My Lead!
+      </Container>
+    </div>
+  );
+};
+
+export default Header
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,10 +5,8 @@ import { Button, Container } from 'reactstrap';
 const Home = () => {
   return (
     <>
-      <HeroImage />
-      <Container className="home-page-headline text-center">Follow My Lead!</Container>
       <div className="register-buttons">
-        <Button  className="button" to="/Register/Owner">
+        <Button className="button" to="/Register/Owner">
           Sign your dog up for walks!
         </Button>
         <Button className="button" to="/Register/Walker">
@@ -16,6 +14,17 @@ const Home = () => {
         </Button>
       </div>
       <div className="home-page-ptag">Partnering People and Pooches!</div>
+      <Container className="blurb-container">
+        <div className="blurb-headline">
+          <h3>Who are we?</h3>
+        </div>
+        <div className="blurb-text">
+          We are a group of web development students who all happen to love
+          dogs! We are working together to improve our development skills by
+          creating a platform for dog lovers that can't own dogs and dog owners
+          that don't always have the time to walk them.
+        </div>
+      </Container>
     </>
   );
 };
