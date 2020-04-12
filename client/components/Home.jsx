708ed4e9ddae8f966a33c7572d87cc392a4e9464
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2020-04-12 17:54:37 +1200

    reformat and style

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -7,15 +7,15 @@ const Home = () => {
     <>
       <HeroImage />
       <Container className="home-page-headline text-center">Follow My Lead!</Container>
-      <div className="home-page-ptag">Partnering People and Pooches!</div>
       <div className="register-buttons">
-        <Button className="button" to="/Register/Owner">
+        <Button  className="button" to="/Register/Owner">
           Sign your dog up for walks!
         </Button>
         <Button className="button" to="/Register/Walker">
           Register to walk a dog!
         </Button>
       </div>
+      <div className="home-page-ptag">Partnering People and Pooches!</div>
     </>
   );
 };
