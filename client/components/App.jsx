5787f9dbb3d030aff45743c0a14974056078a7dc
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2020-04-12 16:54:19 +1200

    complete hero image scaling for mobile

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Home from './Home';
 import { HashRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'reactstrap';
 
 const App = () => {
   return (
     <>
-      <Home />
+      {/* <NavBar/> */}
+      <Home/>
     </>
   );
 };
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import { Button, Container } from 'reactstrap';
+import HeroImage from './BackgroundImage';
 
 const Home = () => {
   return (
     <>
-      <Container className="hero-image">
-        <div className="hero-text">
-          <img
-            className="home-image"
-            src="/images/dog-on-lead.jpeg"
-            alt="hero-dog-image"
-          />
-          {/* <div className="home-page-headline">
-            <p>Follow My Lead!</p>
-          </div>
-          <div className="home-page-ptag">
-            <p>Partnering People and Pooches!</p>
-          </div>
-          <div>
-            <button>Resiter your dog for walks</button>
-            <button>Resiter to walk a dog</button>
-          </div> */}
-        </div>
-      </Container>
+      <HeroImage />
     </>
   );
 };
